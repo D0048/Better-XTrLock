f4d5bed7cef0ef3fa77fabb867eed69c259da0fc
d0048 <1198995013@qq.com>
2017-09-07 16:43:04 +0800

    Add block update algo(half done)
    known issue: xycal

--- mouse_gesture/main.py
+++ mouse_gesture/main.py
@@ -10,20 +10,66 @@ import configparser
 global trigger
 global isGen
 global mouse_x
-mouse_x=1
+global lock_mx
 global mouse_y
-mouse_y=1
-#global configFile
+global lock_my
+global blocks
+mouse_x = 1
+mouse_y = 1
 triggered = False
 
-lock_mx = Lock()  
-counter = Value('i', 0) # int type，相当于java里面的原子变量
-lock_my = Lock()  
-counter = Value('i', 0) # int type，相当于java里面的原子变量
+#lock_mx = Lock()
+#counter = Value('i', 0) # int type，相当于java里面的原子变量
+'''
+block0 = Block()
+block1 = Block()
+block2 = Block()
+block3 = Block()
+block4 = Block()
+block5 = Block()
+block6 = Block()
+block7 = Block()
+block8 = Block()
+block9 = Block()
+block_total = Block()
+'''
+
+blocks = [
+    #block0, block1, block3, block4, block5, block6, block7, block8, block9,
+    #block_total
+]
+
+
+class Block:
+    x1 = 0
+    y1 = 0
+    x2 = 100
+    y2 = 100
+    value = "nah"
+
+    def __init__(self, x1=0, y1=0, x2=100, y2=100, value="nah"):
+        self.x1 = x1
+        self.x2 = x2
+        self.y1 = y1
+        self.y2 = y2
+        self.value = value
+        pass
+
+    def check(self, x, y):
+        if (x >= x1 and x >= x2 and y >= y1 and y <= y2):
+            return True
+        else:
+            return False
+        pass
+
+    def info(self):
+        return "Area:({}, {}) to ({}, {}) with value {}".format(
+            self.x1, self.y1, self.x2, self.y2, self.value)
 
 
 def on_press(key):
     print('{0} pressed'.format(key))
+    global triggered
 
     if (str(key).startswith(trigger)):
         print("triggered")
@@ -33,7 +79,8 @@ def on_press(key):
 
 
 def on_release(key):
-    print('{0} release'.format(key))
+    #print('{0} release'.format(key))
+    global triggered
     if (str(key).startswith(trigger)):
         print("detriggered")
         triggered = False
@@ -44,13 +91,14 @@ def on_move(x, y):
     print('Pointer moved to {0}'.format((x, y)))
     #ohash = hashlib.md5((str(x) + str(y)).encode('utf-8')).hexdigest()
     #print(str(ohash))
-    lock_mx.value = x
-    lock_my.value = y
+    global lock_mx, lock_my
+    lock_mx = x
+    lock_my = y
     pass
 
 
 def on_click(x, y, button, pressed):
-    print('{0} at {1}'.format('Pressed' if pressed else 'Released', (x, y)))
+    #print('{0} at {1}'.format('Pressed' if pressed else 'Released', (x, y)))
     if not pressed:
         # Stop listener
         return False
@@ -86,32 +134,39 @@ def create_default(path):
     pass
 
 
-def main():#TODO:record, hash, display
+def main():  #TODO:record, hash, display
+    global trigger
+    global isGen
+    global mouse_x
+    global lock_mx
+    global mouse_y
+    global lock_my
+    global blocks
     parser = argparse.ArgumentParser(description='xtrlock')
     parser.add_argument(
-            '-t',
-            '--trigger',
-            action='store',
-            type=str,
-            dest="trigger",
-            help="trigger key of the action",
-            default="a")
+        '-t',
+        '--trigger',
+        action='store',
+        type=str,
+        dest="trigger",
+        help="trigger key of the action",
+        default="a")
     parser.add_argument(
-            '-g',
-            '--gen',
-            action='store',
-            type=bool,
-            dest="gen",
-            help="record pwd and generate the config file",
-            default=False)
+        '-g',
+        '--gen',
+        action='store',
+        type=bool,
+        dest="gen",
+        help="record pwd and generate the config file",
+        default=False)
     parser.add_argument(
-            '-c',
-            '--config_file',
-            action='store',
-            type=str,
-            dest="config_file",
-            help="specify the config file to use/write, default as xtrlock.conf",
-            default="xtrlock.conf")
+        '-c',
+        '--config_file',
+        action='store',
+        type=str,
+        dest="config_file",
+        help="specify the config file to use/write, default as xtrlock.conf",
+        default="xtrlock.conf")
 
     args = parser.parse_args()
 
@@ -152,41 +207,85 @@ def main():#TODO:record, hash, display
             cp = configparser.ConfigParser()
             cp.read(config_file, encoding="utf-8-sig")
             update_conf(config_file, cp)
-            cp.write(open(config_file, 'w+', encoding='utf_8_sig'));
+            cp.write(open(config_file, 'w+', encoding='utf_8_sig'))
 
         except Exception as e:
             print(
-                    "Failed to read config file, config backed up at .bak, now creating default: "
-                    + str(e))
-            open(config_file + ".bak", #backup the old broken config
-                    "w+").writelines(open(config_file, "r+"))
+                "Failed to read config file, config backed up at .bak, now creating default: "
+                + str(e))
+            open(
+                config_file + ".bak",  #backup the old broken config
+                "w+").writelines(open(config_file, "r+"))
             create_default(config_file)
     pass
-
 
 
 def update_conf(path, cp):
     #trigger
     new_trigger = input("SizeH(" + cp.get("Setting", "Trigger", raw=True) +
-            "):")
+                        "):")
     print(str(new_trigger))
     if (new_trigger != ''): cp.set("Setting", "Trigger", new_trigger[0])
 
     #SizeX1/Y1
-    input("SizeX1/2/Y1/2(enter to set current mouse position as the first point)")
-    x1=lock_mx.value
-    y1=lock_my.value
-    print("Point 1 Set to:"+str(x1)+', '+str(y1))
+    input("SizeX1/Y1(enter to set current mouse position as the first point)")
+    x1 = lock_mx
+    y1 = lock_my
+    print("Point 1 Set to:" + str(x1) + ', ' + str(y1))
     #SizeX2/Y2
-    input("SizeX1/2/Y1/2(enter to set current mouse position as the second point)")
-    x2=lock_mx.value
-    y2=lock_my.value
-    print("Point 2 Set to:"+str(x2)+', '+str(y2))
+    input("SizeX2/Y2(enter to set current mouse position as the second point)")
+    x2 = lock_mx
+    y2 = lock_my
+    print("Point 2 Set to:" + str(x2) + ', ' + str(y2))
     cp.set("Setting", "SizeX1", str(x1))
     cp.set("Setting", "SizeX2", str(x2))
     cp.set("Setting", "SizeY1", str(y1))
     cp.set("Setting", "SizeY2", str(y2))
+    update_blocks(x1, y1, x2, y2)
+    pass
 
+
+def update_blocks(x1, y1, x2, y2, section_w=3, section_h=3, gap=20):
+    global trigger
+    global isGen
+    global mouse_x
+    global lock_mx
+    global mouse_y
+    global lock_my
+    #regularize x1->x2, y1->y2: 0->100, 0->100 x=l2r,y=u2d
+    if (x1 > x2):
+        x1, x2 = x2, x1
+        print("Auto formated location x")
+        pass
+    if (y1 > y2):
+        y1, y2 = y2, y1
+        print("Auto formated location y")
+        pass
+
+    #find label
+    block_total = Block(x1, y1, x2, y2)
+    blocks.append(block_total)
+    print(block_total.info())
+
+    block_w = int((x1 - x2 - (gap * (section_w - 1))) / (section_w))#TODO:here are two wrong but identical value??
+    block_h = int((x1 - x2 - (gap * (section_h - 1))) / (section_h))
+    print("W per block: {}, H per block: {}".format(block_w, block_h))
+
+    block_value = 0
+    buf_x1, buf_y1, buf_x2, buf_y2 = x1, y1, x1 + block_w, y1 - block_h
+    for ih in range(0, section_h):
+        for iw in range(0, section_w):
+            block = Block(buf_x1, buf_y1, buf_x2, buf_y2, block_value)
+            blocks.insert(0, block)
+            print(block.info())
+            block_value += 1
+            buf_x1 += gap + block_w  #->1
+            pass
+        buf_y1 += 0 - gap - block_h  #br
+        buf_x1 = x1
+        pass
+
+    #TODO: cal blocks and set to global
 
     pass
 
